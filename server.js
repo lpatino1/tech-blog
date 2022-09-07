268dafc6d7b2a4dddba0e8cc3ad73703578e44ff
Lesli <lpatino0994@gmail.com>
2022-09-06 23:34:46 -0500

    added login handlebars, login js and logout functionality

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express =  require('express');
 const session = require ('express-session');
 const expressH = require('express-handlebars');
-const exphbs = require('express-handlebars');i
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
