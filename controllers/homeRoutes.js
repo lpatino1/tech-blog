817f10210c48c74262c0db5f1c08bbf9c6577925
Lesli <lpatino0994@gmail.com>
2022-09-07 00:54:49 -0500

    added dashboard handlebars page, added css styling, fixed navbar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -59,7 +59,7 @@ router.get('/profile', withAuth, async (req, res) => {
   
       const user = userData.get({ plain: true });
   
-      res.render('profile', {
+      res.render('dashboard', {
         ...user,
         logged_in: true
       });
---
+++ public/js/profile.js
@@ -0,0 +1,30 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#project-name').value.trim();
+ 
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blog/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+}
+  
+document
+.querySelector('.new-project-form')
+.addEventListener('submit', newFormHandler);
+
+document
+.querySelector('.project-list')
+.addEventListener('click', delButtonHandler);
